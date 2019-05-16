d740ebe9f3a236070052eb6437435bcc53ad4d60
DentVega <dentvega6@gmail.com>
2019-05-16 18:20:30 -0400

    -  Modification of events.component.html -  Modification of events.component.scss -  Modification of events.model.ts -  Modification of en.json -  Modification of es.json

--- src/app/shared/models/event.model.ts
+++ src/app/shared/models/event.model.ts
@@ -1,5 +1,12 @@
+import { firestore } from 'firebase';
 import { DataType } from './data-type.model';
 
 export interface Event extends DataType {
   id: string;
+  name: string;
+  link: string;
+  place: string;
+  logoUrl: string;
+  description: string;
+  date: firestore.Timestamp;
 }
