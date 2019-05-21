4a7021ac5d7c8fc722b6fed3e9fe7e77024605fc
Giovanni Cespedes <soporte@logicalcrow.com>
2019-05-20 22:53:25 -0400

    correction of the about section

---
+++ src/app/home/about/about.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sw-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent {}
