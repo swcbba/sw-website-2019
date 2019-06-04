77bfd48c33f99649c540f7d93497ca39e4f85462
Griss Garcia <grissgarcia74@gmail.com>
2019-06-04 18:44:10 -0400

    PWA configurations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,20 @@ import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { LayoutModule } from './layout/layout.module';
 import { HomeModule } from './home/home.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 registerLocaleData(localeEs, 'es');
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CoreModule, AppRoutingModule, LayoutModule, HomeModule],
+  imports: [
+    CoreModule,
+    AppRoutingModule,
+    LayoutModule,
+    HomeModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
